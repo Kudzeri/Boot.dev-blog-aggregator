5ca136f2ec00887935961f7af6e866efab14e3e2
Kudzeri <kudzeri@gmail.com>
2024-06-19 20:32:54 +0500

    init router && server

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+)
 
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	portString := os.Getenv("PORT")
+
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Addr:    ":" + portString,
+		Handler: router,
+	}
+
+	fmt.Println("Starting server...")
+	err = srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/Kudzeri/Boot.dev-pokedex-go
 
 go 1.22.0
+
+require github.com/joho/godotenv v1.5.1
+
+require github.com/go-chi/chi v1.5.5
